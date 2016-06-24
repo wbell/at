06ad64e859d5413efabd7bafe96d02ae8e551e25
Willie Bell <willie.bell@pwc.com>
2016-06-24 16:47:18 -0400

    update zip task

--- gulp/tasks/zip.js
+++ gulp/tasks/zip.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 gulp.task('zip', function(){
-  return gulp.src(['dist/*/*'])
+  return gulp.src(['dist/**/*'])
     .pipe(zip('wbell_angular_test_'+(new Date().toISOString())+'.zip'))
     .pipe(gulp.dest('packaged'));
 });
