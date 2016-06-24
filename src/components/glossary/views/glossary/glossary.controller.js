fe480d3016169c90c6443befa958cd0eb7092d5e
Willie Bell <willie.bell@pwc.com>
2016-06-24 09:38:08 -0400

    glossary filters

--- src/components/glossary/views/glossary/glossary.controller.js
+++ src/components/glossary/views/glossary/glossary.controller.js
@@ -8,6 +8,28 @@ var glossaryCtrl = function glossaryCtrl($scope, glossaryCollection){
   // populate glossary items
   $scope.glossaryCollection = glossaryCollection;
 
+  // transform links
+  $scope.transformLinks = function(htmlString){
+    htmlString = htmlString
+      .replace(/="\//gi, '="https://healthcare.gov/') // create absolute URLs
+      .replace(/<a/g, '<a target="_blank"'); // set link target to new window
+
+    return htmlString;
+  };
+
+  // search input model
+  $scope.searchTerm = '';
+  $scope.activeSearchTerm = '';
+  $scope.setTerm = function(term){
+    $scope.activeSearchTerm = term;
+  };
+  // $scope.filterGlossary = function(item){
+  //   return (
+  //     (item.title.toLowerCase().indexOf($scope.activeSearchTerm.toLowerCase()) !== -1) ||
+  //     (item.content.toLowerCase().indexOf($scope.activeSearchTerm.toLowerCase()) !== -1)
+  //   );
+  // };
+
 };
 
 module.exports = glossaryCtrl;
